3430af5fd853f351bfe834d1fb49358523b5dbf0
Utsav-Tyagi <105265047+Utsav-Tyagi@users.noreply.github.com>
2022-05-11 21:53:32 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public claass HelloWorld
 public static void main(String[] args)
 {
 System.out.println("hello world Capgemini git jenkins demo.....");
+System.out.println("hello have fun.....");
+
 
 }
 }
